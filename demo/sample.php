1ef82ac1a72ebe24f3c888e77a000aacbc32de68
Maki <ilulu4ever816@gmail.com>
2014-09-02 11:36:45 +0800

    no message

--- demo/sample.php
+++ demo/sample.php
@@ -5,31 +5,29 @@ $client = new PHPRPC_Client ( 'http://61.145.163.80/biz/interface/xyDataApi.php'
 
 
 /*接收微信活动*/
-// $arr = array(
-// 	'actname' => 'test1',
-// 	'actstart' => '2014-08-28 00:00:00',
-// 	'actend' => '2014-08-29 00:00:00',
-// 	'actgiftnums' => 10
-// );
-// $json = $client->acceptWxActivity( 'XY001JZ', '350001', $arr );
-// $res = json_decode($json);
-// var_dump($res);
+$arr = array(
+	'actname' => 'test2014-08-2800:00:00',
+	'actstart' => '2014-08-28 00:00:00',
+	'actend' => '2014-08-29 00:00:00',
+	'actgiftnums' => 10
+);
+$json = $client->acceptWxActivity( 'XY001JZ', '350001', $arr );
+$res = json_decode($json);
+var_dump($res);
 
 
 
 /*获取达到消费条件的会员列表*/
-$arr = array(
-	'startnum' => 10001,
-	'startdate' => '2014-08-16',
-	'enddate' => '2014-09-01',
-// 	'minamount' => 0,
-// 	'maxamount' => 1000,
-	'constimes' => 1,
-);
-$json = $client->getConsCusList( 'XY001JZ', '350001', $arr );
-echo $json;
-$res = json_decode($json);
-var_dump($res);
+// $arr = array(
+// 	'startnum' => 10001,
+// 	'startdate' => '2014-08-16',
+// 	'enddate' => '2014-09-01',
+// 	'constimes' => 1,
+// );
+// $json = $client->getConsCusList( 'XY001JZ', '350001', $arr );
+// echo $json;
+// $res = json_decode($json);
+// var_dump($res);
 
 
 
