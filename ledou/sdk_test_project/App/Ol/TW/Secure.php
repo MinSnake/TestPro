2cfd96846a01da1031bebed9a0bd1a2e1a475545
saki <zha_zha@outlook.com>
2017-11-03 19:01:03 +0800

    test

--- ledou/sdk_test_project/App/Ol/TW/Secure.php
+++ ledou/sdk_test_project/App/Ol/TW/Secure.php
@@ -6,6 +6,19 @@ use Lib\App\BaseApp;
 Class Secure extends BaseApp
 {
 
+    /**
+    http://svn.ids111.com:81/dgc_payment/source/taiwan_tag/tw_payment-trunk
+
+
+    http://svn.ids111.com/dgc/dgc_sdkfeed_online/program/atrunk_taiwan/olfeed_TW-trunk
+
+    http://svn.ids111.com/dgc/dgc_secure_online/program/secure_TW_tag/dgc_TW_secure_online-trunk
+     *
+     * 微信AppID：wxff23efc69058a60f 
+    微信AppSecret：90c211a20d47fe47bb92a3d4f6057280 
+     PackageName：tw.com.idealgame.yld 
+     *
+     */
 
     public function oauth_request_token()
     {
